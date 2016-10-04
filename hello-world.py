ecb4799c0bd68d5f1650bd886768a802be52d966
Mohamed Sahad KP <mohamed.sahad@experionglobal.com>
2016-10-05 02:03:01 +0530

    machine learning hello world

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+from sklearn import tree
+#features = [[140, "smooth"], [130, "smooth"], [150,"bumpy"], [170, "bumby"]]
+#labels = ["apple", "apple", "orange", "orange"]
+
+#smooth = 0, bumpy = 1
+#apple = 0, orange = 1
+
+features = [[140, 0], [130, 0], [150, 1], [170, 1]]
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print clf.predict([[150, 0]])
