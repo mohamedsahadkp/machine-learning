22ece2457b69c6a1b86f8ef9a2d196d1e5692784
Mohamed Sahad KP <mohamed.sahad@experionglobal.com>
2017-01-29 20:01:39 +0530

    updated study and matk

--- scikit-learn/study-sleep-linear-regression.py
+++ scikit-learn/study-sleep-linear-regression.py
@@ -5,31 +5,16 @@
     system predict how much mark he will score in the exame 
     """
 
-# from sklearn import linear_model
+from sklearn import linear_model
 
-# #number of hour sleep and study
-# feature = [[3, 5], [5, 1], [10, 2]]
-
-# #mark secured
-# label = [75, 82, 93]
-
-# reg = linear_model.LinearRegression()
-# reg.fit(feature, label)
-
-# result = reg.predict([8, 3])
-# print result
-
-
-from sklearn.linear_model import LinearRegression
-
-#number of hour study
-feature = [5, 6, 4, 1, 10]
+#number of hour sleep and study
+feature = [[3, 5], [5, 1], [10, 2]]
 
 #mark secured
-label = [75, 80, 70, 40, 85]
+label = [75, 82, 93]
 
-reg = LinearRegression()
+reg = linear_model.LinearRegression()
 reg.fit(feature, label)
 
-result = reg.predict(3)
+result = reg.predict([8, 3])
 print result
